e760fcf4855978ac09e5b0c9c3450f673e9597e0
SlemimanPzz <emilianoa@ciencias.unam.mx>
2023-02-27 10:21:14 -0600

    Initial commit

---
+++ app/src/main/java/mx/unam/fciencias/practica2pdm/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.unam.fciencias.practica2pdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
